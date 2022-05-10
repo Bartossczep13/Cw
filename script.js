7464f07b5dbb4c2b053d9ee581d13f03ee85e7b8
TeO <100691829+Bartossczep13@users.noreply.github.com>
2022-05-10 11:38:52 +0000

    5_3_wypisz_1

--- script.js
+++ script.js
@@ -36,7 +36,20 @@
   })
 
   cw3.addEventListener("click", function () {
-    //TODO implement it
+    answer.innerHTML = "Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML = "";
+      for (var i = 0; i <1; i++){
+        var div = document.createElement("div");
+        div.innerHTML= '<h1>UserID: ' + data[i].userId + '</h1> <strong>id: ' + data[i].id + '</strong> <br> <strong>title: </strong>' + data[i].title + '<br> <h2>body:</h2> '+ data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
   })
 
 })();
